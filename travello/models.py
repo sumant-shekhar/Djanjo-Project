0345a83694388449b710083ce06e8a7a0de0248c
sumant-shekher <sumantshekhar.dev@gmail.com>
2026-05-13 12:19:34 +0530

    DBMS and dynamic location added

---
+++ travello/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='media')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
